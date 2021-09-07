60bf68e42ed22068e71ebb9a9d3c4c972ff2bbdb
Damián Martínez <A00832849@itesm.mx>
2021-09-07 12:05:35 -0500

    correccion licencia

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,22 +1,23 @@
-
 def main():
-  #comentario
 
- edad = int(input("Ingresa tu edad: "))
- if edad >=18:
-    identi = input("¿Tienes identificación oficial? (s/n): ")  
-    if  identi == 's'or identi == 'S':
-     print('Trámite de licencia concedido')
-
-    elif identi == 'n' or identi == 'N':
-     print('No cumples requisitos')
-
+    edad = int(input("Ingresa tu edad: "))
+    
+    
+    if edad >= 18:
+        identi = input("¿Tienes identificación oficial? (s/n): ")
+        if  identi == 's'or identi == 'S':
+            print("Trámite de licencia concedido")
+        elif identi == 'n' or identi == 'N':
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+            
+               
     else:
-      print('Respuesta incorrecta') 
- elif edad >= 0 and edad < 18:
-   print('No cumples requisitos')
- else:
-   print('Respuesta incorrecta')
-   
+        if edad <= 0:
+            print("Respuesta incorrecta")
+        else:
+            print("No cumples requisitos")
+
 if __name__ == '__main__':
     main()
